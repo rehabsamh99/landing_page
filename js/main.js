f164a33b08aa54faa62d6b6863d82bb317ff027e
Rehab Sameh <161374560+rehabsamh99@users.noreply.github.com>
2024-10-11 20:01:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+const btnOpenVideo = document.querySelectorAll('.islands_video_content')
+const islandsPopup = document.getElementById('popup')
+
+function poPup(){
+    islandsPopup.classList.add('show-popup')
+}
+btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
+
+const btnCloseVideo = document.getElementById('islands_popup_close')
+
+btnCloseVideo.addEventListener('click', ()=> {
+    islandsPopup.classList.remove('show-popup')
+})
+
+
+
+
+
+//**********swiper********//
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
+    slidesPerView: 4,
+})
+
+let swiper2 = new Swiper(".mySwiper2", {
+        effect:'fade',
+        loop : true ,
+    thumbs: {
+        swiper: swiper,
+    },
+})
